295f20d50f3c92c550deffaf8d3f570708d6c92e
AndestaDewi <andestadewi05@gmail.com>
2026-01-20 21:33:24 +0700

    membuat layout file menggunakan blade components

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,6 +8,12 @@
 </head>
 
 <body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+
     <h1>this is the about page</h1>
 </body>
 
