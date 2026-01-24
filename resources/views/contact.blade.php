792934985c1074dff58341459660d9556c1c22c2
AndestaDewi <andestadewi05@gmail.com>
2026-01-24 15:06:55 +0700

    membuat layout menggunakan tailwind css

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
-</head>
-
-<body>
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-    </nav>
-
+<x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+    
     <h1>this is the contact page</h1>
-</body>
-
-</html>
+</x-layout>
